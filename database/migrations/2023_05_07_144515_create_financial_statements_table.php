13ec3afa5b929e8da181e1372f2f3d00496f2ebe
ninahidayah <ninaahidayahh@gmail.com>
2023-05-07 21:58:25 +0700

    make migrations

---
+++ database/migrations/2023_05_07_144515_create_financial_statements_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('financial_statements', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_pemasukkan');
+            $table->foreign('id_pemasukkan')->references('id')->on('incomes');
+            $table->unsignedBigInteger('id_pengeluaran');
+            $table->foreign('id_pengeluaran')->references('id')->on('expenditures');
+            $table->unsignedBigInteger('id_hutang');
+            $table->foreign('id_hutang')->references('id')->on('debts');
+            $table->date('Tanggal_Laporan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('financial_statements');
+    }
+};
