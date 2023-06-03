20d806da61c1097cce70c577f9af70bfb4fdd1f6
FanielSianipar <fanielsianipar01@gmail.com>
2023-06-04 03:53:12 +0700

    icon

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -2,62 +2,61 @@
 
     <ul class="sidebar-nav" id="sidebar-nav">
 
-      <li class="nav-item">
-        <a class="nav-link " href="{{ url('/admin') }}">
-          <i class="bi bi-grid"></i>
-          <span>Dashboard</span>
-        </a>
-      </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{ url('/admin') }}">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ url('/jabatan') }}">
-          <i class="bi bi-newspaper"></i>
-          <span>Jabatan</span>
-        </a>
-      </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{ url('/jabatan') }}">
+                <i class="bi bi-newspaper"></i>
+                <span>Jabatan</span>
+            </a>
+        </li>
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ url('/karyawan') }}">
-          <i class="bi bi-newspaper"></i>
-          <span>Data Karyawan</span>
-        </a>
-      </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{ url('/karyawan') }}">
+                <i class="bi bi-people-fill"></i>
+                <span>Data Karyawan</span>
+            </a>
+        </li>
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ url('/keuangan') }}">
-          <i class="bi bi-box-arrow-in-up"></i>
-          <span>Keuangan</span>
-        </a>
-      </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{ url('/keuangan') }}">
+                <i class="bi bi-cash-coin"></i>
+                <span>Keuangan</span>
+            </a>
+        </li>
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ url('/laporan') }}">
-          <i class="bi bi-layout-text-window-reverse"></i>
-          <span>Laporan Keuangan</span>
-        </a>
-      </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{ url('/laporan') }}">
+                <i class="bi bi-journals"></i>
+                <span>Laporan Keuangan</span>
+            </a>
+        </li>
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ url('/profileAdmin') }}">
-          <i class="bi bi-layout-text-window-reverse"></i>
-          <span>Profile</span>
-        </a>
-      </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{ url('/profileAdmin') }}">
+                <i class="bi bi-person-circle"></i>
+                <span>Profile</span>
+            </a>
+        </li>
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ url('/login') }}">
-          <i class="bi bi-box-arrow-in-right"></i>
-          <span>Login</span>
-        </a>
-      </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{ url('/login') }}">
+                <i class="bi bi-box-arrow-in-right"></i>
+                <span>Login</span>
+            </a>
+        </li>
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ url('/register') }}">
-          <i class="bi bi-card-list"></i>
-          <span>Register</span>
-        </a>
-      </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{ url('/register') }}">
+                <i class="bi bi-card-list"></i>
+                <span>Register</span>
+            </a>
+        </li>
     </ul>
 
-  </aside>
-
+</aside>
